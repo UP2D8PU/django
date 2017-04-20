0e31c4c95fe78168f4387b29bdc95a0abbd4f2d9
dbpro2017 <toragunstad@gmail.com>
2017-04-20 19:52:07 +0200

    faen

--- prosjekt1/settings.py
+++ prosjekt1/settings.py
@@ -133,4 +133,18 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = '/Users/toragunstad/Desktop/prosjekt1/prosjekt1/staticfiles'
+#os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
